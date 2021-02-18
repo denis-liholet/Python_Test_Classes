7ece780ecf367b5a62dc80d7c4258d87300e1062
Denis Liholet <palloncino@ukr.net>
2021-02-18 09:20:29 +0200

    replace position of lists init

--- test_preparation.py
+++ test_preparation.py
@@ -2,17 +2,18 @@
 
 INPUT_FILE_PATH = 'questions.txt'
 
-questions = list()
-temp_list = list()
-correct_answers = list()
-
 
 def preparation():
-    """ читает строки из файла с вопросами теста, возвращает три списка:
+    """ читает строки из файла с вопросами теста, возвращает кортеж из трёх списков:
     1) список вопросов, 2) список вариантов ответов, 3) список с правильными ответами
     """
 
     with open(INPUT_FILE_PATH, "r", encoding="UTF-8") as my_file:
+
+        questions = list()
+        options_list = list()
+        correct_answers = list()
+        temp_list = list()
 
         for line in my_file:
             questions.append(line.strip())
@@ -21,15 +22,13 @@ def preparation():
             temp_list.append(my_file.readline().strip())
             correct_answers.append(int(my_file.readline().split(":")[1].strip()))
 
-        options = list()
-
         # формирует элементы списка вариантов ответов, создавая один элемент
-        # нового списка "options" из трёх элементов временного списка "temp_list"
+        # нового списка "options_list" из трёх элементов временного списка "temp_list"
         for i in range(0, len(temp_list), 3):
-            options.append(
-                            temp_list[i] + "\n" +
-                            temp_list[i + 1] + "\n" +
-                            temp_list[i + 2]
-                           )
+            options_list.append(
+                temp_list[i] + "\n" +
+                temp_list[i + 1] + "\n" +
+                temp_list[i + 2]
+            )
 
-    return questions, options, correct_answers
+    return questions, options_list, correct_answers
